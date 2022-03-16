c35de3c33bd472e7b1409a13cc4c3a94a4c79291
Choiyeojin <choiyj0303@naver.com>
2022-03-16 11:08:47 +0900

    upload

---
+++ src/main/java/Exam01.java
@@ -0,0 +1,17 @@
+public class Exam01 {
+    public static void main(String[] args) {
+
+        System.out.println("최여진");
+        System.out.println("030305");
+        System.out.println("java");
+        System.out.println("psvm은 메인");
+        System.out.println("sout은 시스템프린트ln");
+        System.out.println("폴리텍 데이터분석과 1학년은 일주일 내내 1교시");
+        System.out.println("오늘은 수요일");
+        System.out.println("한이음 공모전");
+        System.out.println("파스타 공모전");
+        System.out.println("오늘진도는 자바 책 88p");
+
+
+    }
+}
