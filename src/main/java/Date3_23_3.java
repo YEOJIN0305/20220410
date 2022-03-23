720f80468bb18113dd57cd953695d6a6855c9372
Choiyeojin <choiyj0303@naver.com>
2022-03-23 10:46:02 +0900

    Date3_23_upload

---
+++ src/main/java/Date3_23_3.java
@@ -0,0 +1,20 @@
+public class Date3_23_3 {
+    public static void main(String[] args) {
+        int a, b = 5, c = 3;//Ctrl Alt L 은 줄맞춤
+
+        a = b + c;
+        System.out.printf("%d + %d = %d \n", b, c, a);
+
+        a = b - c;
+        System.out.printf("%d - %d = %d \n", b, c, a);
+
+        a = b * c;
+        System.out.printf("%d * %d = %d \n", b, c, a);
+
+        a = b / c;
+        System.out.printf("%d / %d = %d \n", b, c, a);
+
+        a = b % c;
+        System.out.printf("%d %% %d = %d \n", b, c, a);
+    }
+}
