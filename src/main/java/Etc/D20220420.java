35b9f965e94247faf785bc70f1d63855db2aa995
Choiyeojin <choiyj0303@naver.com>
2022-04-26 16:52:55 +0900

    2022_04_26

---
+++ src/main/java/Etc/D20220420.java
@@ -0,0 +1,15 @@
+package Etc;
+
+public class D20220420 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i = 50; i<101; i++){
+            if ((i%3==0)||(i%7==0)||(i%11==0)){
+
+            }else{
+                sum = sum +i;
+            }
+        }
+        System.out.print(sum);
+    }
+}
