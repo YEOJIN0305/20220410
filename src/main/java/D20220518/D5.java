81cded75fc3aeb255247608bff44e55494d695ca
Choiyeojin <choiyj0303@naver.com>
2022-05-30 00:20:39 +0900

    2022_05_30

---
+++ src/main/java/D20220518/D5.java
@@ -0,0 +1,17 @@
+package D20220518;
+
+public class D5 {
+    public static void main(String[] args) {
+
+        String str = "IT CookBook. Java";
+        int len;
+
+        len = str.length();
+
+        System.out.printf("문자열 : %s \n", str);
+        System.out.printf("문자열 길이 : %d ", len);
+
+    }
+
+
+}
