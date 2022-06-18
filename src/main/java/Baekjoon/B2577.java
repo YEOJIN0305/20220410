9453d0304b6e2be0f3320788d23c6a4724ad7426
Choiyeojin <choiyj0303@naver.com>
2022-06-18 20:17:02 +0900

    2022_06_18

---
+++ src/main/java/Baekjoon/B2577.java
@@ -0,0 +1,15 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class B2577 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+
+        int sum = a*b*c;
+    }
+}
