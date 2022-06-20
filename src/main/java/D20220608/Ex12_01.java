ed42171c2a39780cde6172771c6af54ebcb69fbc
Choiyeojin <choiyj0303@naver.com>
2022-06-20 12:58:02 +0900

    2022_06_20

---
+++ src/main/java/D20220608/Ex12_01.java
@@ -0,0 +1,48 @@
+package D20220608;
+
+class Car2 {
+    String color;
+    int speed;
+
+    void upSpeed(int value){
+        speed = speed+value;
+    }
+
+    void downSpeed(int value){
+        speed = speed -value;
+    }
+}
+
+class Sedan extends Car2 {
+    int seatNum;
+
+    int getSeatNum(){
+        return seatNum;
+    }
+}
+
+class Truck extends Car2{
+    int capacity;
+
+    int getCapacity(){
+        return capacity;
+    }
+}
+
+public class Ex12_01 {
+    public static void main(String[] args) {
+
+        Sedan sedan1 = new Sedan();
+        Truck truck1 = new Truck();
+
+        sedan1.upSpeed(300);
+        truck1.upSpeed(100);
+
+        sedan1.seatNum = 5;
+        truck1.capacity = 50;
+
+        System.out.println("승용차의 속도는 "+sedan1.speed+"km, 좌석수는 "+sedan1.getSeatNum()+"개 입니다");
+        System.out.println("트럭의 속도는 "+truck1.speed+"km, 적재량은 "+truck1.getCapacity()+"톤 입니다");
+    }
+
+}
